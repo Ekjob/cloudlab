7367b2745e8281a56d1ab42ef574747605497ace
ekjob <bojke101@hotmail.com>
2022-11-05 21:16:40 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("terraform-330619-58e299c355c3.json")
+
+  project = "terraform-330619"
+  region  = "europe-west4"
+  zone    = "europe-west4-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      #image = "debian-cloud/debian-9"
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_tags   = ["web"]
+  source_ranges = ["86.93.136.90/32"]
+}
